37d2385c9a62d6456ee132a59ecfea4385cb11b1
thwhitfield <thwhitfield@gmail.com>
2019-07-25 23:43:40 -0400

    added house_prices kaggle competition

---
+++ 02_house_prices/spyder_house_prices.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 25 17:44:18 2019
+
+@author: thwhi
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
